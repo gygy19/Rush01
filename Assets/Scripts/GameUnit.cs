c04a1d8a86b90649f6e5060db9816920f7312911
Felix PAGE <fpage@e1r11p5.42.fr>
2017-06-24 01:02:39 +0200

    changement gameUnit

--- Assets/Scripts/GameUnit.cs
+++ Assets/Scripts/GameUnit.cs
@@ -4,29 +4,47 @@ using UnityEngine;
 
 public class GameUnit : MonoBehaviour {
 
+	const int MULTIPLICATEURSTRHP = 2;
+	const int MULTIPLICATEURINTELMANA = 2;
+
 	private int hp;
 	private int exp = 0;
 	private int mana;
+	private float timeRegenMana = 0;
 	[SerializeField]
-	private int str, agi, intel, maxHp, regenMana, manaMax;
+	private int str, agi, intel, hpBase, regenMana, manaBase;
 
-	void Start () {
-		this.hp = this.maxHp;
-		this.mana = this.manaMax;
+	protected void Start () {
+		this.hp = this.getMaxHp();
+		this.mana = this.getMaxMana();
 	}
 	
-	void Update () {
-		if (this.mana < this.manaMax) {
-			if (this.mana + this.regenMana <= this.manaMax)
-				this.mana += this.regenMana;
-			else
-				this.mana = manaMax;
+	protected void Update () {
+		if (this.timeRegenMana >= 1) {
+			this.timeRegenMana = 0;
+			if (this.mana < this.getMaxMana ()) {
+				if (this.mana + this.regenMana <= this.getMaxMana ())
+					this.mana += this.regenMana;
+				else
+					this.mana = getMaxMana ();
+			}
+			Debug.Log ("mana");
 		}
+		this.timeRegenMana += Time.deltaTime;
 	}
-
 	public int getHp()
 	{
 		return this.hp;
+	}
+
+	public int getMaxHp()
+	{
+		return this.hpBase + this.str * MULTIPLICATEURSTRHP;
+	}
+
+	public int getMaxMana()
+	{
+		return this.manaBase + this.str * MULTIPLICATEURINTELMANA;
 	}
 
 	public int getStr()
--- Assets/Scripts/Player/RPGPlayer.cs
+++ Assets/Scripts/Player/RPGPlayer.cs
@@ -6,12 +6,13 @@ public class RPGPlayer : GameUnit {
 
 	// Use this for initialization
 	void Start () {
+		base.Start ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		base.Update ();
 		if (Input.GetKey(KeyCode.A))
 			this.addExp(10);
-		Debug.Log (this.getLevel ());
 	}
 }
