35e1c962307868ff59c195cb50ec93f2d361b2c6
Jeremy GUYET <jguyet@e1r10p2.42.fr>
2017-06-24 08:34:15 +0200

    begin spell UI

---
+++ Assets/Scripts/UI/UISpellsScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISpellsScript : MonoBehaviour {
+
+	public ButtonScript exitButton;
+	public GameObject[] contentObjects;
+	public bool visible = false;
+
+	// Use this for initialization
+	void Start () {
+		foreach (GameObject o in contentObjects) {
+			o.SetActive (false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (exitButton.isSelected) {
+			exitButton.isSelected = false;
+			setVisible (false);
+		}
+	}
+
+	public void setVisible(bool visible) {
+		this.visible = visible;
+		foreach (GameObject o in contentObjects) {
+			o.SetActive (visible);
+		}
+	}
+}
