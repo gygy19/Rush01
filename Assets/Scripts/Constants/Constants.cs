d8bd69bec06285463177f4aa085a3ba2f7bd6f24
Felix PAGE <fpage@e1r11p1.42.fr>
2017-06-25 01:08:59 +0200

    meta

---
+++ Assets/Scripts/Constants/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants {
+
+	public static Vector3 GetMousePosition()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit[] hits = Physics.RaycastAll (ray);
+		foreach (RaycastHit hitInfo in hits)
+		{
+			if (hitInfo.collider.gameObject.name == "Cemetery")
+				return (hitInfo.point); 
+		}
+		return Vector3.zero;
+	}
+
+}
