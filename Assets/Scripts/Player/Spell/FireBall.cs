bf190cc4a1fe857773686fee41a6abcf51eeb9bc
Felix PAGE <fpage@e1r11p1.42.fr>
2017-06-25 01:41:17 +0200

    description boule de feu

--- Assets/Scripts/Player/Spell/FireBall.cs
+++ Assets/Scripts/Player/Spell/FireBall.cs
@@ -6,6 +6,9 @@ using DigitalRuby.PyroParticles;
 public class FireBall : Spell {
 
 	public GameObject ball;
+	public int damage;
+	public int variationDamage;
+	public int LevelUpBoost;
 
 	void Start () {
 		base.Start ();
@@ -20,12 +23,17 @@ public class FireBall : Spell {
 		Vector3 pos = RPGPlayer.Player.transform.position;
 		pos.y += 4;
 		GameObject fire = GameObject.Instantiate(ball, pos, Quaternion.LookRotation(Constants.GetMousePosition() - RPGPlayer.Player.transform.position));
-
 		RPGPlayer.Player.GetComponent<PlayerController> ().StopMovement ();
 		RPGPlayer.Player.transform.rotation = Quaternion.LookRotation(Constants.GetMousePosition() - RPGPlayer.Player.transform.position);
+	}
 
+	public override string getValue()
+	{
+		return "[" + (damage - variationDamage) + " - " + (damage + variationDamage) + "]";
 	}
 
 	public override string getUpgrade()
-	{return null;}
+	{
+		return "[" + (damage - variationDamage + LevelUpBoost * this.getLevel()) + " - " + (damage + variationDamage + LevelUpBoost * this.getLevel()) +  "]";
+	}
 }
