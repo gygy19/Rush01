a76223b7245d8af4a359dac61b8988e22de704a7
Felix PAGE <fpage@e1r11p1.42.fr>
2017-06-25 08:58:35 +0200

    repush item

---
+++ Assets/Scripts/Player/Item/ItemsTypes/PotionVie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionVie : Item {
+
+	// Use this for initialization
+	void Start () {
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.Update ();
+	}
+
+	public override bool use()
+	{
+		RPGPlayer player = RPGPlayer.Player.GetComponent<RPGPlayer> ();
+		Debug.Log ("utilisation potion");
+		return (player.addHp ((int)(player.getMaxHp() * 0.3f)));
+	}
+}
