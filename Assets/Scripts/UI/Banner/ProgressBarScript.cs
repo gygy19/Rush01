1097f236bafc5c8fd7344ef3832f881349511157
Jeremy GUYET <jguyet@e1r10p2.42.fr>
2017-06-25 02:47:49 +0200

    Progress Barres

---
+++ Assets/Scripts/UI/Banner/ProgressBarScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBarScript : MonoBehaviour {
+
+	private Vector3 baseScale;
+	private Vector3 basePosition;
+	private RectTransform rect;
+
+	// Use this for initialization
+	void Start () {
+		rect = this.GetComponent<RectTransform> ();
+		baseScale = new Vector3 (this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+		basePosition = new Vector3 (rect.position.x, rect.position.y, rect.position.z);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void UpdatePurcent(float purcent) {
+		float width = rect.sizeDelta.x;
+
+		float purcentscale = (purcent / 100f);
+		this.transform.localScale = new Vector3 (purcentscale, this.transform.localScale.y, this.transform.localScale.z);
+		rect.position = new Vector3 ((basePosition.x - ((width / 2) - (((width / 2) * purcent) / 100))),basePosition.y, basePosition.z);
+	}
+}
